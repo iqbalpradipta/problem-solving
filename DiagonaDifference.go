6b50d9257a8a354ac59029c668fe08078141bff2
iqbalpradipta <iqbalpradipta2@gmail.com>
2022-10-27 18:07:00 +0700

    problem-solving-solution

---
+++ DiagonaDifference.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'diagonalDifference' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+ */
+
+func diagonalDifference(arr [][]int32) int32 {
+    // Write your code here
+    var diagonalpertama int32
+    var diagonalkedua int32
+
+    for i := 0; i < len(arr); i++ {
+        for j := 0; j < len(arr); j++ {
+            if i == j {
+                diagonalpertama += arr[i][j]
+            }
+            if i + j == len(arr)-1{
+                diagonalkedua += arr[i][j]
+            }
+        }
+    }
+    diagonal := diagonalpertama - diagonalkedua
+
+    if diagonal < 0 {
+        diagonal = -diagonal
+    }
+    return diagonal
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    stdout, err := os.Create(os.Getenv("OUTPUT_PATH"))
+    checkError(err)
+
+    defer stdout.Close()
+
+    writer := bufio.NewWriterSize(stdout, 16 * 1024 * 1024)
+
+    nTemp, err := strconv.ParseInt(strings.TrimSpace(readLine(reader)), 10, 64)
+    checkError(err)
+    n := int32(nTemp)
+
+    var arr [][]int32
+    for i := 0; i < int(n); i++ {
+        arrRowTemp := strings.Split(strings.TrimRight(readLine(reader)," \t\r\n"), " ")
+
+        var arrRow []int32
+        for _, arrRowItem := range arrRowTemp {
+            arrItemTemp, err := strconv.ParseInt(arrRowItem, 10, 64)
+            checkError(err)
+            arrItem := int32(arrItemTemp)
+            arrRow = append(arrRow, arrItem)
+        }
+
+        if len(arrRow) != int(n) {
+            panic("Bad input")
+        }
+
+        arr = append(arr, arrRow)
+    }
+
+    result := diagonalDifference(arr)
+
+    fmt.Fprintf(writer, "%d\n", result)
+
+    writer.Flush()
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
---
+++ MiniMaxSum.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "bufio"
+    // "fmt"
+    "io"
+    "os"
+    "strconv"
+    "strings"
+)
+
+/*
+ * Complete the 'miniMaxSum' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+func miniMaxSum(arr []int32) {
+    // Write your code here
+    var result int32
+    for i := 0; i < len(arr); i++ {
+        if i - 1 == 14 {
+            result += arr[i]
+        } else if i - 2 == 13{
+            result += arr[i]
+        } else if i - 3 == 12 {
+            result += arr[i]
+        } else if i - 4 == 11 {
+            result += arr[i]
+        } else if i - 5 == 10 {
+            result += arr[i]
+        }
+    }
+}
+
+func main() {
+    reader := bufio.NewReaderSize(os.Stdin, 16 * 1024 * 1024)
+
+    arrTemp := strings.Split(strings.TrimSpace(readLine(reader)), " ")
+
+    var arr []int32
+
+    for i := 0; i < 5; i++ {
+        arrItemTemp, err := strconv.ParseInt(arrTemp[i], 10, 64)
+        checkError(err)
+        arrItem := int32(arrItemTemp)
+        arr = append(arr, arrItem)
+    }
+
+    miniMaxSum(arr)
+}
+
+func readLine(reader *bufio.Reader) string {
+    str, _, err := reader.ReadLine()
+    if err == io.EOF {
+        return ""
+    }
+
+    return strings.TrimRight(string(str), "\r\n")
+}
+
+func checkError(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
